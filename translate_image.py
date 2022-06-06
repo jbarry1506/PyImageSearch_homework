daaad253dca95a4accf6619fb26e3b4f7c9350b7
Jim Barry <jbarry1506@gmail.com>
2022-06-06 17:25:31 -0400

    lesson 3

---
+++ translate_image.py
@@ -0,0 +1,26 @@
+import numpy as np
+import imutils
+import cv2
+import argparse
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", type=str, default="C:\\Users\\Black Beauty\\Pictures\\LI Motivation\\captain_pirate.jpg", 
+    help="path to the picture you want to work with")
+
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+
+cv2.imshow("Original", image)
+
+M = np.float32([[1, 0, 25], [0, 1, 50]])
+shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+cv2.imshow("shifted down and right", shifted)
+cv2.waitKey(0)
+
+M = np.float32([[1, 0, -10], [0, 1, -20]])
+shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+cv2.imshow("image shifted up and left", shifted)
+cv2.waitKey(0)
+
