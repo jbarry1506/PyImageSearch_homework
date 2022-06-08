0dd1fd6e550b2dc8a1e3567252a3b9b4b985980b
Jim Barry <jbarry1506@gmail.com>
2022-06-08 01:54:24 -0400

    Lesson 12

---
+++ split_channels.py
@@ -0,0 +1,23 @@
+# Lesson 12
+
+
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", type=str, default="opencv.png",
+    help="Image to split colors.")
+
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+
+(B,G,R) = cv2.split(image)
+
+cv2.imshow("Blue Channel", B)
+cv2.imshow("Green Channel", G)
+cv2.imshow("Red Channel", R)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
