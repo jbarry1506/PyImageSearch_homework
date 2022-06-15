8c3a2216efc4caf82333b27f70d2f329514bf4be
Jim Barry <jbarry1506@gmail.com>
2022-06-15 00:23:29 -0400

    OpenCV 102 Lesson 2-B

--- OpenCV_102/bilateral.py
+++ OpenCV_102/bilateral.py
@@ -2,3 +2,22 @@
 # Bilateral Smoothing and Blurring
 
 import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+
+ap.add_argument("-i", "--image", type=str, default="scum-villainy.jpg")
+
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+
+# (diameter, sigma_color, sigma_space)
+params = [(11,21,7), (11,41,21), (11,61,39)]
+
+for (diameter, sigma_color, sigma_space) in params:
+    blurred = cv2.bilateralFilter(image, diameter, sigmaColor=sigma_color, sigmaSpace=sigma_space)
+    title = f"Blurred {diameter},{sigma_color},{sigma_space}"
+    cv2.imshow(title, blurred)
+    cv2.waitKey(0)
