7d2fb40d4c4dcaa7c59011ee3180fe3af00ac8c9
Jim Barry <jbarry1506@gmail.com>
2022-06-29 19:34:49 -0400

    Lesson 3

--- OpenCV_104/lesson_3.py
+++ OpenCV_104/lesson_3.py
@@ -29,3 +29,25 @@ cv2.imshow("Source", src)
 cv2.imshow("Reference", ref)
 cv2.imshow("Matched", matched)
 cv2.waitKey(0)
+
+
+# my plots aren't working right
+(fig, axs) = plt.subplots(nrows=3, ncols=3, figsize=(8,8))
+
+for (i, image) in enumerate((src, ref, matched)):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+for (j, color) in enumerate(("red", "green", "blue")):
+    (hist, bins) = exposure.histogram(image[..., j],
+        source_range="dtype")
+    axs[j, i].plot(bins, hist / hist.max())
+
+    (cdf, bins) = exposure.cumulative_distribution(image[..., j])
+    axs[j, 0].set_ylabel(color)
+
+axs[0, 0].set_title("Source")
+axs[0, 1].set_title("Reference")
+axs[0, 2].set_title("Matched")
+
+plt.tight_layout()
+plt.show()
